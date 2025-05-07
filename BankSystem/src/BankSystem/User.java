e90b555aaf4847812fd9c5be3752eb3f2621c0ae
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-05-07 18:42:18 +0545

    User details

--- BankSystem/src/BankSystem/User.java
+++ BankSystem/src/BankSystem/User.java
@@ -3,27 +3,50 @@ package BankSystem;
 public class User {
     String username;
     String password;
+    String accountNumber;
     double balance;
 
-    public User(String username, String password) {
+    public User(String username, String password,String accountNumber) {
         this.username = username;
         this.password = password;
+        this.accountNumber = accountNumber;
         this.balance = 0.0;
     }
 
-    public void deposit(double amount) {
-        balance += amount;
+    public String getUsername() {
+        return username;
     }
 
-    public boolean withdraw(double amount) {
-        if (amount <= balance) {
-            balance -= amount;
-            return true;
-        }
-        return false;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public double getBalance() {
         return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+    public void displayUserAccountDetails(){
+        System.out.println("Balance: "+getBalance());
+        System.out.println("Account Number: "+getAccountNumber());
+        System.out.println("userName: "+getUsername());
     }
 }
