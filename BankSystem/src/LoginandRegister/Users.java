cb30e51f7fbcc3ae5efc702658328911c3fa1cdb
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-05-09 22:20:12 +0545

    bank management system change

--- BankSystem/src/LoginandRegister/Users.java
+++ BankSystem/src/LoginandRegister/Users.java
@@ -4,11 +4,13 @@ public class Users {
     private String name;
     private String userName;
     private String password;
+    private String accountNumber;
 
-    public Users(String name, String userName, String password) {
+    public Users(String name, String userName, String password, String accountNumber) {
         this.name = name;
         this.userName = userName;
         this.password = password;
+        this.accountNumber = accountNumber;
     }
 
     public String getName() {
@@ -21,5 +23,17 @@ public class Users {
 
     public String getPassword() {
         return password;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: "+name+","+
+                "UserName: "+userName+","+
+                "Password: "+password+","+
+                "Account Number: "+accountNumber;
     }
 }
