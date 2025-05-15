44b66921324879e12070c0b29147967bbf0a0f6e
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-05-15 22:01:14 +0545

    change on BankManagement system

--- BankSystem/src/Services/BanksServices.java
+++ BankSystem/src/Services/BanksServices.java
@@ -6,14 +6,13 @@ import LoginandRegister.LoginRegister;
 import java.util.List;
 
 public interface BanksServices {
+    void Adduser(User user);
     void Login(LoginRegister loginRegister);
 
     void register(LoginRegister loginRegister);
 
-    void UserAccount(User user, String accountNumber, String userName);
-
     void depositAmount(String accountNumber, String userName, double amount);
     void Withdraw(String accountNumber, String userName,double amount);
 
-    void displayDetails(String loginUser);
+    void displayDetails(String username,String password,String accountNumber);
 }
