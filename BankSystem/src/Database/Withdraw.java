d7dc2b0b501eb7cb959b0dd3b6d69a6eb5f4a952
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-06-16 16:12:40 +0545

    bank management system

---
+++ BankSystem/src/Database/Withdraw.java
@@ -0,0 +1,44 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Withdraw {
+    public static void withdraw(String username,double amount){
+        try (Connection connection = DBConnection.getConnection()) {
+            String query = "SELECT balance FROM user_details WHERE user_name = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, username);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+
+
+
+            if (resultSet.next()) {
+                double balance = resultSet.getDouble("balance");
+
+                String query2 = "UPDATE user_details SET balance = balance - ? WHERE user_name = ?";
+                PreparedStatement preparedStatement1 = connection.prepareStatement(query2);
+                preparedStatement1.setDouble(1, amount);
+                preparedStatement1.setString(2, username);
+
+                int row = preparedStatement1.executeUpdate();
+                if (row > 0) {
+                    System.out.println("✅ Rs. " + amount + " deposited successfully!");
+                    System.out.println("💰 New Balance: Rs. " + (balance - amount));
+                } else {
+                    System.out.println("❌ Failed to update balance.");
+                }
+
+            } else {
+                System.out.println("❌ Account not found!");
+            }
+
+        } catch (SQLException e) {
+            System.out.println("🔴 Error: " + e.getMessage());
+        }
+    }
+}
+
